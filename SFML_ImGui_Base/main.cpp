5c1b51178409054b98b51f9e1a53c85a8e7f18ab
Björn Winnerstam <brevbomb@hotmail.com>
2017-03-31 13:40:05 +0200

    Made a changable circle

--- SFML_ImGui_Base/main.cpp
+++ SFML_ImGui_Base/main.cpp
@@ -5,6 +5,7 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
+#include <SFML\Graphics.hpp>
 
 #include <iostream>
 #include <time.h>
@@ -12,11 +13,30 @@
 #include <vector>
 
 
+static void ShowHelpMarker(const char* desc)
+{
+	ImGui::TextDisabled("(?)");
+	if (ImGui::IsItemHovered())
+	{
+		ImGui::BeginTooltip();
+		ImGui::PushTextWrapPos(450.0f);
+		ImGui::TextUnformatted(desc);
+		ImGui::PopTextWrapPos();
+		ImGui::EndTooltip();
+	}
+}
+
 int main()
 {
+	
+
 	static int HEIGHT = 600;
 	static int WIDTH = 800;
 	static int BORDER_ON_TOP = 20;
+	static int dragSize = 50;
+	static int dragX = WIDTH / 2 - dragSize;
+	static int dragY = HEIGHT / 2 - dragSize;
+
 
 	srand((unsigned int)time(NULL));
 
@@ -49,18 +69,7 @@ int main()
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			// this will get the position of the mouse ON THE DESKTOP
-			sf::Vector2i mPos = sf::Mouse::getPosition();
-			// this will get the position of the WINDOW
-			sf::Vector2i wPos = window.getPosition();
-
-			// calculate if the mouse is on the window, but only max 20 pixels from the top 
-			int mousePosition = mPos.y - wPos.y;
-			if (mousePosition > 0 && mousePosition < BORDER_ON_TOP)
-				redyToMove = true;
-			else
-				redyToMove = false;
-
+			ImGui::SFML::ProcessEvent(event);
 
 			if (event.type == sf::Event::Closed)
 			{
@@ -75,6 +84,17 @@ int main()
 			}
 			else if (event.type == sf::Event::MouseButtonPressed)
 			{
+				// this will get the position of the mouse ON THE DESKTOP
+				sf::Vector2i mPos = sf::Mouse::getPosition();
+				// this will get the position of the WINDOW
+				sf::Vector2i wPos = window.getPosition();
+
+				// calculate if the mouse is on the window, but only max 20 pixels from the top 
+				int mousePosition = mPos.y - wPos.y;
+				if (mousePosition > 0 && mousePosition < BORDER_ON_TOP)
+					redyToMove = true;
+				else
+					redyToMove = false;
 				// redyToMove will only be true if the mouse is over the top border
 				if(redyToMove)
 				if (event.mouseButton.button == sf::Mouse::Left)
@@ -95,26 +115,50 @@ int main()
 			}
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-			ImGui::SFML::ProcessEvent(event);
 		}
 
 		ImGui::SFML::Update(window, deltaClock.restart());
 
+
+
+		
+
+
 		// Setting next window to full screen and with a static size and position
-		ImGui::SetNextWindowSize(ImVec2(WIDTH, HEIGHT));
+	/*	ImGui::SetNextWindowSize(ImVec2(WIDTH, HEIGHT));
 		ImGui::SetNextWindowSizeConstraints(ImVec2(WIDTH, HEIGHT), ImVec2(WIDTH, HEIGHT));
-		ImGui::SetNextWindowPos(ImVec2(0, 0));
+		ImGui::SetNextWindowPos(ImVec2(0, 0));*/
 
 		ImGui::Begin(u8"Mitt Fönster"); // begin window
+
+		if (ImGui::Button(u8"Återställ", ImVec2(100, 30)))
+		{
+			dragSize = 50;
+			dragX = WIDTH / 2 - dragSize;
+			dragY = HEIGHT / 2 - dragSize;
+		}
+		
+		ImGui::DragInt("Circle size", &dragSize, 1);
+		ImGui::DragInt("Circle pos.x", &dragX, 1);
+		ImGui::DragInt("Circle pos.y", &dragY, 1);
+		ImGui::SameLine(); ShowHelpMarker("Click and drag to edit value.\nHold SHIFT/ALT for faster/slower edit.\nDouble-click or CTRL+click to input value.");
 
 
 		ImGui::End(); // end window
 
-				    //ImGui::ShowTestWindow();
+		//ImGui::ShowTestWindow();
+
+		sf::CircleShape circle;
+		circle.setRadius(dragSize);
+		circle.setOutlineColor(sf::Color::Red);
+		circle.setOutlineThickness(5);
+		circle.setPosition(dragX, dragY);
+
 
 
 
 		window.clear(bgColor); // fill background with color
+		window.draw(circle);
 		ImGui::Render();
 		window.display();
 	}
